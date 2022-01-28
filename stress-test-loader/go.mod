5ab421ccf3db5b9a77cefd819de17e8af4109694
Jack Xie <jxie@seconddinner.com>
2022-01-27 18:19:55 -0800

    add s3 handling

--- stress-test-loader/go.mod
+++ stress-test-loader/go.mod
@@ -3,6 +3,7 @@ module stress-test-loader
 go 1.17
 
 require (
+	github.com/aws/aws-sdk-go v1.42.43
 	github.com/sirupsen/logrus v1.8.1
 	google.golang.org/grpc v1.44.0
 	google.golang.org/protobuf v1.27.1
@@ -10,8 +11,9 @@ require (
 
 require (
 	github.com/golang/protobuf v1.5.0 // indirect
-	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
-	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
-	golang.org/x/text v0.3.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+	golang.org/x/text v0.3.6 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 )
--- stress-test-loader/main.go
+++ stress-test-loader/main.go
@@ -12,6 +12,10 @@ import (
 
 	pb "stress-test-loader/proto"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 )
@@ -25,8 +29,39 @@ type server struct {
 	pb.LoadTestLoaderServer
 }
 
-func startLoadTest(in *pb.TestRequest) {
+func copyStressTest(in *pb.TestRequest) (err error) {
+	file, err := os.Create("/tmp/cli_s3.tgz")
+	if err != nil {
+		log.Error("Unable to open file %q, %v", in.S3Key, err)
+	}
+	defer file.Close()
+
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-west-2")},
+	)
+
+	downloader := s3manager.NewDownloader(sess)
+
+	numBytes, err := downloader.Download(file,
+		&s3.GetObjectInput{
+			Bucket: aws.String(in.S3),
+			Key:    aws.String(in.S3Key),
+		})
+	if err != nil {
+		log.Error("Unable to download in.S3Key %q, %v", in.S3Key, err)
+	}
+
+	log.Println("Downloaded", file.Name(), numBytes, "bytes")
+	return
+}
+
+func startLoadTest(in *pb.TestRequest) (err error) {
 	log.Print(in)
+
+	err = copyStressTest(in)
+	if err != nil {
+		return
+	}
 	cmd := exec.Command(in.LoadtestExec)
 	cmd.Env = os.Environ()
 	for _, s := range in.EnvVariableList {
@@ -36,9 +71,9 @@ func startLoadTest(in *pb.TestRequest) {
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
 
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
-		log.Print("cmd.Run: %s failed: %s\n", err)
+		log.Error("cmd.Run: %s failed: %s\n", err)
 	}
 	outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
 	if len(errStr) > 1 {
