43f7d1c919c6dfde45282d76fffadd2599a0710f
Jack Xie <jxie@seconddinner.com>
2022-01-27 21:31:11 -0800

    packer working

--- stress-test-loader/cicd/ami/build-stress-test-loader.sh
+++ stress-test-loader/cicd/ami/build-stress-test-loader.sh
@@ -4,4 +4,5 @@ echo "${STL_version}"
 packer init cicd/packer
 packer fmt cicd/packer
 packer build -var="PNS_version=hmp-proxy-cluster-${STL_version}" -machine-readable cicd/packer/ubuntu-stress-test-loader.pkr.hcl | tee build.log
-# export PROXY_NODE_SELFCHECK_AMI_ID=`grep 'artifact,0,id' build.log | cut -d, -f6 | cut -d: -f2`
+export PROXY_NODE_SELFCHECK_AMI_ID=`grep 'artifact,0,id' build.log | cut -d, -f6 | cut -d: -f2`
+set +x
