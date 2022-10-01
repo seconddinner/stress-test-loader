bc8169624f4e60e7d474987b90a60f45462cf615
Jack Xie <97316601+jxie-sd@users.noreply.github.com>
2022-09-30 22:38:00 -0700

    split out elk stack (#9)

--- infra-terraform/instance/single-region-elk/output.tf
+++ infra-terraform/instance/single-region-elk/output.tf
@@ -7,6 +7,15 @@ output "vpc_id" {
 output "subnet_ids" {
   value = data.aws_subnets.self.ids
 }
+output "es_endpoint" {
+  value = var.create_esdomain ? module.es-domain[0].es_endpoint : data.aws_elasticsearch_domain.es[0].endpoint
+}
+output "es_kibana_endpoint" {
+  value = var.create_esdomain ? module.es-domain[0].kibana_endpoint : data.aws_elasticsearch_domain.es[0].kibana_endpoint
+}
+output "es_masterusername" {
+  value = var.masterusername
+}
 output "stressclient_asg_name" {
   value = module.autoscale.stressclient_asg_name
 }
