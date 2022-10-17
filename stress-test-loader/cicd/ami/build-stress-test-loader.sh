f4e2e9b80e7eeb3f4d6cb7938596660c52bedd6f
Jack Xie <jxie@seconddinner.com>
2022-10-16 21:06:30 -0700

    git descript

--- stress-test-loader/cicd/ami/build-stress-test-loader.sh
+++ stress-test-loader/cicd/ami/build-stress-test-loader.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -x 
-echo "hello world"
-# export STL_version=`git describe --tags --long`
-# echo "${STL_version}"
-# source build.sh
+export STL_version=`git describe --tags --long`
+echo "${STL_version}"
+bash build.sh
 # packer init cicd/packer
 # packer fmt cicd/packer
 # packer build -var="PNS_version=stress-test-loader-${STL_version}" -machine-readable cicd/packer/ubuntu-stress-test-loader.pkr.hcl | tee build.log
