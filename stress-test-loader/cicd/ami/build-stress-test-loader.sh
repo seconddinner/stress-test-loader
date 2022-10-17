45991953c259a5ac8292a3f758476f14bae0ec60
Jack Xie <jxie@seconddinner.com>
2022-10-16 21:01:38 -0700

    why can't build

--- stress-test-loader/cicd/ami/build-stress-test-loader.sh
+++ stress-test-loader/cicd/ami/build-stress-test-loader.sh
@@ -2,9 +2,9 @@
 set -x 
 export STL_version=`git describe --tags --long`
 echo "${STL_version}"
-source build.sh
-packer init cicd/packer
-packer fmt cicd/packer
-packer build -var="PNS_version=stress-test-loader-${STL_version}" -machine-readable cicd/packer/ubuntu-stress-test-loader.pkr.hcl | tee build.log
-export PROXY_NODE_SELFCHECK_AMI_ID=`grep 'artifact,0,id' build.log | cut -d, -f6 | cut -d: -f2`
-set +x
+# source build.sh
+# packer init cicd/packer
+# packer fmt cicd/packer
+# packer build -var="PNS_version=stress-test-loader-${STL_version}" -machine-readable cicd/packer/ubuntu-stress-test-loader.pkr.hcl | tee build.log
+# export PROXY_NODE_SELFCHECK_AMI_ID=`grep 'artifact,0,id' build.log | cut -d, -f6 | cut -d: -f2`
+# set +x
