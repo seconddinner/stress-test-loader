07bc889d7e089dc1babfad8457092b55874872ae
Jack Xie <jxie@seconddinner.com>
2022-10-16 21:03:43 -0700

    why can't build v2

--- stress-test-loader/cicd/ami/build-stress-test-loader.sh
+++ stress-test-loader/cicd/ami/build-stress-test-loader.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -x 
-export STL_version=`git describe --tags --long`
-echo "${STL_version}"
+echo "hello world"
+# export STL_version=`git describe --tags --long`
+# echo "${STL_version}"
 # source build.sh
 # packer init cicd/packer
 # packer fmt cicd/packer
 # packer build -var="PNS_version=stress-test-loader-${STL_version}" -machine-readable cicd/packer/ubuntu-stress-test-loader.pkr.hcl | tee build.log
 # export PROXY_NODE_SELFCHECK_AMI_ID=`grep 'artifact,0,id' build.log | cut -d, -f6 | cut -d: -f2`
-# set +x
+set +x
