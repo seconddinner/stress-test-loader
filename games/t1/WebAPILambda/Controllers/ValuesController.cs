f59b4d997f2af2ff157f57bb43914c73826cd4da
Jack Xie <97316601+jxie-sd@users.noreply.github.com>
2023-01-20 11:02:00 -0800

    pulumi lambda aspdotnet integration example with github action. (#19)
    * add yml
    
    * update pulumi
    
    * init
    
    * use newer version pulumi
    
    * update version of pulumi
    
    * working dotnet function
    
    * update Docker to include zip
    
    * need to commit two places
    
    * net6.0
    
    * remove branch
    
    * add concurrency so we don't run into pulumi lock
    
    * update PR

---
+++ games/t1/WebAPILambda/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using WebAPILambda.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPILambda.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return $"Hello World from inside a lambda Jack {DateTime.Now}";
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"You asked for {id}";
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            return Ok($"You sent {person.ToString()}");
+        }
+    }
+}
