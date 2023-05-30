e2c78742b88b3c471209fb83dce64abaa5b4889a
Wei Wu <wwu@seconddinner.com>
2023-05-30 16:36:35 -0400

    add a simple stress test client
    This stress test client is written in dotnet. It pings a url (e.g., www.google.com) for num_pings times, where num_pings is an environment variable.

---
+++ simple-stress-test-client/StressTest/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Net.NetworkInformation;
+
+class Program
+{
+    static void Main()
+    {
+        int numPings;
+        string numPingsEnv = Environment.GetEnvironmentVariable("num_pings");
+
+        if (!int.TryParse(numPingsEnv, out numPings) || numPings <= 0)
+        {
+            Console.WriteLine("Invalid value for num_pings environment variable. Using default value: 10");
+            numPings = 10;
+        }
+
+        string logFile = "/tmp/stress-test-log";
+        string host = "www.google.com";
+        Ping pingSender = new Ping();
+
+        try
+        {
+            using (StreamWriter sw = new StreamWriter(logFile))
+            {
+                Console.SetOut(sw);
+
+                for (int i = 0; i < numPings; i++)
+                {
+                    PingReply reply = pingSender.Send(host);
+
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        Console.WriteLine($"Ping {i + 1} to {host} was successful.");
+                        Console.WriteLine($"Roundtrip time: {reply.RoundtripTime}ms");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Ping {i + 1} to {host} failed. Status: {reply.Status}");
+                    }
+                }
+            }
+        }
+        catch (PingException ex)
+        {
+            using (StreamWriter sw = new StreamWriter(logFile))
+            {
+                Console.SetOut(sw);
+                Console.WriteLine($"An exception occurred while pinging {host}: {ex.Message}");
+            }
+        }
+    }
+}
