e26ca710308e328f4f91385a73c3ad14c669fd6c
Wei Wu <wwu@seconddinner.com>
2023-05-30 18:45:46 -0400

    fix a bug when env var "environment" is not set

--- infra-pulumi/Infra.Pulumi/Infra.Pulumi/DeployHelpers.cs
+++ infra-pulumi/Infra.Pulumi/Infra.Pulumi/DeployHelpers.cs
@@ -86,7 +86,15 @@ public static class DeployHelpers
         string sourceAmiId = Environment.GetEnvironmentVariable("source_ami_id");
         string localPublicIp = Environment.GetEnvironmentVariable("stress_test_loader_allowed_cidr");
         string publicKey = Environment.GetEnvironmentVariable("public_key");
-        string environment = Environment.GetEnvironmentVariable("environment") ?? "stresstest-git-action";
+        string environment;
+        try
+        {
+            environment = Environment.GetEnvironmentVariable("environment");
+        }
+        finally
+        {
+            environment = "stresstest-git-action";
+        }
         var config = new Dictionary<string, ConfigValue>
         {
             { "aws:region", new ConfigValue("us-west-2") },
