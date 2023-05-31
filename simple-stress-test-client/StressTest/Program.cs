2701dcb76b6fbf6d266b2b23cac9044605b9df52
Wei Wu <wwu@seconddinner.com>
2023-05-31 12:00:38 -0400

    add ping_interval and host to client

--- simple-stress-test-client/StressTest/Program.cs
+++ simple-stress-test-client/StressTest/Program.cs
@@ -15,8 +15,23 @@ class Program
             numPings = 10;
         }
 
+        int pingInterval;
+        string pingIntervalEnv = Environment.GetEnvironmentVariable("ping_interval");
+
+        if (!int.TryParse(pingIntervalEnv, out pingInterval) || pingInterval <= 0)
+        {
+            Console.WriteLine("Invalid value for ping_interval environment variable. Using default value: 1000ms");
+            pingInterval = 1000;
+        }
+
         string logFile = "/tmp/stress-test-log";
-        string host = "www.google.com";
+        string host = Environment.GetEnvironmentVariable("host"); // Get the host from an environment variable
+        if (string.IsNullOrEmpty(host))
+        {
+            Console.WriteLine("Host not specified. Using default host: www.google.com");
+            host = "www.google.com";
+        }
+
         Ping pingSender = new Ping();
 
         try
@@ -27,16 +42,22 @@ class Program
 
                 for (int i = 0; i < numPings; i++)
                 {
+                    DateTime currentTime = DateTime.Now; // Get the current system time
                     PingReply reply = pingSender.Send(host);
 
                     if (reply.Status == IPStatus.Success)
                     {
-                        Console.WriteLine($"Ping {i + 1} to {host} was successful.");
+                        Console.WriteLine($"{currentTime}: Ping {i + 1} to {host} was successful.");
                         Console.WriteLine($"Roundtrip time: {reply.RoundtripTime}ms");
                     }
                     else
                     {
-                        Console.WriteLine($"Ping {i + 1} to {host} failed. Status: {reply.Status}");
+                        Console.WriteLine($"{currentTime}: Ping {i + 1} to {host} failed. Status: {reply.Status}");
+                    }
+
+                    if (i < numPings - 1)
+                    {
+                        System.Threading.Thread.Sleep(pingInterval);
                     }
                 }
             }
