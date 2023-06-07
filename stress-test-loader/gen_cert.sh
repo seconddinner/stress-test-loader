91d9fefb2a1f3616ed9c7dffedbe17a25affea51
Wei Wu <wwu@seconddinner.com>
2023-06-07 17:28:46 -0400

    update gen_cert.sh to generate CA's cert

--- stress-test-loader/gen_cert.sh
+++ stress-test-loader/gen_cert.sh
@@ -7,21 +7,24 @@ cp cert/ca-key.pem client/cert/ca-key.pem
 cp cert/ca-cert.pem client/cert/ca-cert.pem
 cd cert
 
-# 1. Generate web server's private key and certificate signing request (CSR)
+# 1. Generate CA's certificate (given the CA's private key at ca-key.pem)
+openssl req -new -x509 -key ca-key.pem -out ca-cert.pem -days 365 -subj "/O=seconddinner"
+
+# 2. Generate web server's private key and certificate signing request (CSR)
 echo "Generating server's private key and CSR"
 openssl req -newkey rsa:4096 -nodes -keyout server-key.pem -out server-req.pem -subj "/O=seconddinner"
 
-# 2. Use CA's private key to sign web server's CSR
+# 3. Use CA's private key to sign web server's CSR
 echo "Signing server's CSR"
 openssl x509 -req -in server-req.pem -days 60 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem
 openssl x509 -in server-cert.pem -noout -text
 
 cd ../client/cert
-# 3. Generate client's private key and certificate signing request (CSR)
+# 4. Generate client's private key and certificate signing request (CSR)
 echo "Generating client's private key and CSR"
 openssl req -newkey rsa:4096 -nodes -keyout client-key.pem -out client-req.pem -subj "/O=seconddinner"
 
-# 4. Use CA's private key to sign client's CSR
+# 5. Use CA's private key to sign client's CSR
 echo "Signing client's CSR"
 openssl x509 -req -in client-req.pem -days 60 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out client-cert.pem
 openssl x509 -in client-cert.pem -noout -text
